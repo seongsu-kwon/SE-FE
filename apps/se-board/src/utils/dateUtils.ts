01f2d99c9e45de8271d0254d35f2e53a63653272
brobac <issg2030@gmail.com>
2023-04-03 22:35:41 +0900

    Fix : typo

--- apps/se-board/src/utils/dateUtils.ts
+++ apps/se-board/src/utils/dateUtils.ts
@@ -1,7 +1,7 @@
 import { DateType } from "@types";
 import dayjs from "dayjs";
 export const toYYMMDD_DOT = (date: DateType) => {
-  return dayjs(date).format("YY.MM.DD.");
+  return dayjs(date).format("YY.MM.DD");
 };
 
 export const isSameDateTime = (date1: DateType, date2: DateType) => {
