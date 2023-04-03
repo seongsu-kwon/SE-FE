1f335d335e11909e43e0769abb5afa71d8c17db2
brobac <issg2030@gmail.com>
2023-04-03 23:21:35 +0900

    Feat : Desktop용 헤더

--- apps/se-board/src/pages/notice/NoticePage.tsx
+++ apps/se-board/src/pages/notice/NoticePage.tsx
@@ -1,13 +1,37 @@
-import { Hide } from "@chakra-ui/react";
+import { Flex, Heading, Hide, Img, Show, Stack } from "@chakra-ui/react";
+
+import NoticeIcon from "@/assets/images/notice_icon.png";
 
 import { NoticeList } from "./NoticeList";
-
+import { NoticeTable } from "./NoticeTable";
 export const NoticePage = () => {
   return (
-    <div>
+    <Stack alignItems="center" maxW="1180px" w="full" px="1rem" py="3rem">
+      <Show above="md">
+        <Flex
+          position="relative"
+          alignItems="center"
+          maxW="container.sm"
+          w="full"
+          px="2rem"
+          py="1.2rem"
+          bgColor="gray.1"
+          borderRadius="0.5rem"
+        >
+          <Heading fontSize="2rem">공지사항</Heading>
+          <Img
+            src={NoticeIcon}
+            position="absolute"
+            w="6rem"
+            right="2rem"
+            top="-1.5rem"
+          />
+        </Flex>
+        <NoticeTable />
+      </Show>
       <Hide above="md">
         <NoticeList />
       </Hide>
-    </div>
+    </Stack>
   );
 };
