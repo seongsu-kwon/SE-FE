f3f8e2202fc9d5ea617a000f710d476ef762cbfb
brobac <issg2030@gmail.com>
2024-01-28 20:00:35 +0900

    페이지네이션 수정

--- apps/se-board/src/pages/board/BoardPage.tsx
+++ apps/se-board/src/pages/board/BoardPage.tsx
@@ -101,7 +101,7 @@ export const BoardPage = () => {
             <PostTable
               data={postList}
               totalItems={totalItems}
-              perPage={40}
+              perPage={1}
               page={page}
             />
           )}
