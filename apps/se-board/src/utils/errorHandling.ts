5f029bd4f5de5bf37887fc317d54835603692bcf
lcy <8410294@naver.com>
2024-03-10 21:06:46 +0900

    Add: Error code

--- apps/se-board/src/utils/errorHandling.ts
+++ apps/se-board/src/utils/errorHandling.ts
@@ -6,10 +6,11 @@ export const incorrectPostPassword = () => {
 
 const errorCodeToAlert = [
   103, 109, 115, 117, 118, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131,
-  132, 133, 134, 135, 136, 137, 138, 140, 141, 142, 143, 144, 202, 203, 204,
-  301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 400,
+  132, 133, 134, 135, 136, 137, 138, 140, 141, 142, 143, 144, 145, 202, 203,
+  204, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 400,
 ];
 const errorCodeToAlertAndGoToBack = [104, 200, 201, 300];
+const errorCodeToAlertAndMain = [146];
 
 export function errorHandle(error: ErrorCode | unknown) {
   const { code, message } = error as ErrorCode;
@@ -19,6 +20,9 @@ export function errorHandle(error: ErrorCode | unknown) {
   } else if (errorCodeToAlertAndGoToBack.includes(code)) {
     alert(message);
     window.history.back();
+  } else if (errorCodeToAlertAndMain.includes(code)) {
+    alert(message);
+    location.replace("/");
   } else if (code === 139) {
     alert("댓글 작성에 실패했습니다. 다시 시도해주세요.");
     location.reload();
