d175ad056d6902e765e380ef3c661836de30ed7a
brobac <issg2030@gmail.com>
2024-03-31 18:24:11 +0900

    모바일 스크린 사이즈에서 게시물 상세 조회 시에도 헤더를 노출시키도록 변경

--- apps/se-board/src/components/layouts/MainLayout.tsx
+++ apps/se-board/src/components/layouts/MainLayout.tsx
@@ -1,20 +1,16 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
 import { DesktopHeaderNavigation } from "@/components";
 import { menuListState } from "@/store/menu";
-import { mobileHeaderState } from "@/store/mobileHeaderState";
 
 export const MainLayout = () => {
   const MenuList = useRecoilValue(menuListState);
-  const mobileOpen = useRecoilValue(mobileHeaderState);
 
   return (
     <>
-      <Box display={{ base: mobileOpen ? "block" : "none", md: "block" }}>
-        <DesktopHeaderNavigation menuList={MenuList} />
-      </Box>
+      <DesktopHeaderNavigation menuList={MenuList} />
       <Flex justifyContent="center">
         <Outlet />
       </Flex>
