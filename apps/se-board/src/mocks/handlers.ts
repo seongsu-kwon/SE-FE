43036069f9d3a878a7f5d2ffebc829177d582405
hodu42 <knn330@naver.com>
2024-08-09 17:49:24 +0900

    Test: handlers.ts passwordChangeWithoutLoginHandlers.ts 임포트

--- apps/se-board/src/mocks/handlers.ts
+++ apps/se-board/src/mocks/handlers.ts
@@ -2,10 +2,12 @@ import { putLoginLimitTimeHandler } from "./loginHandlers";
 import { mainpageHandlers } from "./mainpageHandlers";
 import { menuHandlers } from "./menuHandlers";
 import { mypageHandlers } from "./mypageHandlers";
+import { passwordChangeWithoutLoginHandlers } from "./passwordChangeWithoutLoginHandlers";
 
 export const handlers = [
   ...putLoginLimitTimeHandler,
   ...mainpageHandlers,
   ...menuHandlers,
   ...mypageHandlers,
+  ...passwordChangeWithoutLoginHandlers,
 ];
