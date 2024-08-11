e4c612a51a6327bdb627a96b27a722db2e4d7d2e
seongsukwon <starriver99@naver.com>
2024-08-11 20:48:43 +0900

    test: auth 테스트코드

--- apps/se-board/src/mocks/handlers.ts
+++ apps/se-board/src/mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { accountHandlers } from "./accountHandlers";
 import { adminHandlers } from "./adminHandlers";
+import { authHandlers } from "./authHandlers";
 import { putLoginLimitTimeHandler } from "./loginHandlers";
 import { memberManageHandlers } from "./memberManageHandlers";
 import { postManageHandlers } from "./postManageHandlers";
@@ -10,4 +11,5 @@ export const handlers = [
   ...postManageHandlers,
   ...accountHandlers,
   ...adminHandlers,
+  ...authHandlers,
 ];
