ba768a9ab6ef986b3580e73be48ddc310557756c
seongsukwon <starriver99@naver.com>
2024-08-11 20:46:55 +0900

    test: postManage 테스트코드

--- apps/se-board/src/mocks/handlers.ts
+++ apps/se-board/src/mocks/handlers.ts
@@ -1,4 +1,9 @@
 import { putLoginLimitTimeHandler } from "./loginHandlers";
 import { memberManageHandlers } from "./memberManageHandlers";
+import { postManageHandlers } from "./postManageHandlers";
 
-export const handlers = [...putLoginLimitTimeHandler, ...memberManageHandlers];
+export const handlers = [
+  ...putLoginLimitTimeHandler,
+  ...memberManageHandlers,
+  ...postManageHandlers,
+];
